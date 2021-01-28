5c1c45433f69a1009430d1be01f1d3de229807cb
Neville Bokdawalla <neville.bokdawalla@bookmyshow.com>
2021-01-29 01:55:16 +0530

    module names as per use case - job aggregator added test and error files using echo middleware

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/kr/pretty v0.2.1 // indirect
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/echo/v4 v4.1.17
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/stretchr/testify v1.5.1 // indirect
--- server/server.go
+++ server/server.go
@@ -2,14 +2,22 @@ package server
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/neville/go-web-api-app-template/server/routes"
 )
 
 // Start ...
 func Start() {
+	// Create instance
 	e := echo.New()
 
+	// Register middlewares
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Register routes
 	routes.Register(e)
 
-	e.Start(":8080")
+	// Start HTTP server
+	e.Logger.Fatal(e.Start(":8080"))
 }
